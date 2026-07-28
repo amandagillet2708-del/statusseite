e400c51095baf2e70b21122291eea1da2ea331ec
amandagillet2708-del <amanda.gillet2708@t-online.de>
2026-07-29 00:23:58 +0100

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const url = req.query.url;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).json({ error: "Proxy Fehler" });
+  }
+}
